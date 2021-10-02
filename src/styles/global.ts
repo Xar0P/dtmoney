81f00d84c14f6c7d9ee67660b5b996840548b597
Xar0P <pietroricardocres@hotmail.com>
2021-10-02 17:54:06 -0300

    components Header & DashBoard

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,53 +1,54 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-    :root {
-        --background: #f0f2f5;
-        --red: #e52e4d;
-        --blue: #5429cc;
-        --blue-light: #6933ff;
-        --text-title: #363f5f;
-        --text-body: #969cb3;
-        --background: #f0f2f5;
-        --shape: #ffffff;
-    }
-    
-    * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-    }
+	:root {
+		--background: #f0f2f5;
+		--red: #e52e4d;
+		--green: #33cc95;
+		--blue: #5429cc;
+		--blue-light: #6933ff;
+		--text-title: #363f5f;
+		--text-body: #969cb3;
+		--background: #f0f2f5;
+		--shape: #ffffff;
+	}
 
-    html {
-        @media (max-width: 1080px) { // Diminuir tamanho da fonte no celular
-            font-size: 93.75%; // 15px
-        }
+	* {
+		 margin: 0;
+		padding: 0;
+		box-sizing: border-box;
+	}
 
-        @media (max-width: 720px) {
-            font-size: 87.5%; // 14px
-        }
-    }
+	html {
+		@media (max-width: 1080px) { // Diminuir tamanho da fonte no celular
+			font-size: 93.75%; // 15px
+		}
 
-    body {
-        background-color: var(--background);
-        -webkit-font-smoothing: antialiased; // Deixar as fontes mais nitidas
-    }
+		@media (max-width: 720px) {
+			font-size: 87.5%; // 14px
+		}
+	}
 
-    body, input, textarea, button {
-        font-family: 'Poppins', sans-serif;
-        font-weight: 400;
-    }
+	body {
+		background-color: var(--background);
+		-webkit-font-smoothing: antialiased; // Deixar as fontes mais nitidas
+	}
 
-    h1, h2, h3, h4, h5, h6, strong {
-        font-weight: 600;
-    }
+	body, input, textarea, button {
+		font-family: 'Poppins', sans-serif;
+		font-weight: 400;
+	}
 
-    button {
-        cursor: pointer;
-    }
+	h1, h2, h3, h4, h5, h6, strong {
+		font-weight: 600;
+	}
 
-    [disabled] {
-        opacity: 0.6;
-        cursor: not-allowed;
-    }
+	button {
+		cursor: pointer;
+	}
+
+	[disabled] {
+		opacity: 0.6;
+		cursor: not-allowed;
+	}
 `
