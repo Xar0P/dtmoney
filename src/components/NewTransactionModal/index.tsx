4268cdfab5851bea58c8666ff22f3a7038e012d2
Xar0P <pietroricardocres@hotmail.com>
2021-10-05 10:58:47 -0300

    format data & finish

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,10 +1,10 @@
 import { FormEvent, useState, useContext } from 'react';
-import { TransactionsContext } from '../../TransactionsContext';
 import Modal from 'react-modal';
 import { Container, TransactionTypeContainer, RadioBox } from './styles';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import closeImg from '../../assets/close.svg';
+import { useTransactions } from '../../hooks/useTransactions';
 
 Modal.setAppElement('#root');
 
@@ -14,7 +14,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-	const { createTransaction } = useContext(TransactionsContext);
+	const { createTransaction } = useTransactions();
 
 	const [title, setTitle] = useState('');
 	const [amount, setAmount] = useState(0);
